00ffca844b071ffc4bb9ca12fd5409a6e2c8565e
Laura Grassi <42256702+lauragrassig@users.noreply.github.com>
2019-11-07 14:57:24 -0300

    Alteração index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
+// import React from 'react';
+// import { render } from "react-dom";
+// import { BrowserRouter, Route } from "react-router-dom";
+// import language from "./pages/language";
+// import cards from "./pages/cards";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <div>
+//           <Route path="/" exact component={language} />
+//           <Route path="/cards" component={cards} />
+//         </div>
+//       </BrowserRouter>
+//     );
+//   }
+// }
+
+// render(<App />, document.getElementById("root"));
+
+
 import React from 'react';
 import { render } from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import language from "./pages/language";
-import cards from "./pages/cards";
+import Language from "./pages/language";
+import Cards from "./pages/cards";
+import Header from "./components/header";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={language} />
-          <Route path="/cards" component={cards} />
+          <Route exact path="/">
+            <Header />
+            <Language />
+          </Route>
+          <Route path="/cards">
+            <Cards />
+          </Route>
         </div>
       </BrowserRouter>
     );
